7496c9d6c72e1f2e0a182cd897d1a7753f2d8e18
calthas <bertrand.dunogier@mail.com>
2010-04-07 20:01:20 +0200

    Enhanced output handing for linkbyartist.php

--- linkbyartist.php
+++ linkbyartist.php
@@ -9,44 +9,69 @@ $input = new ezcConsoleInput();
 $dryRunOption = $input->registerOption( new ezcConsoleOption( 'd', 'dry-run', ezcConsoleInput::TYPE_NONE, false ) );
 $quietOption = $input->registerOption( new ezcConsoleOption( 'q', 'quiet', ezcConsoleInput::TYPE_NONE, false ) );
 $helpOption = $input->registerOption( new ezcConsoleOption( 'h', 'help' ) );
+$verboseOption = $input->registerOption( new ezcConsoleOption( 'v', 'verbose', ezcConsoleInput::TYPE_NONE, false ) );
 
 $input->argumentDefinition = new ezcConsoleArguments();
 $input->argumentDefinition[0] = new ezcConsoleArgument( "artist" );
-$input->argumentDefinition[0]->mandatory = false;
+$input->argumentDefinition[0]->mandatory = true;
 $input->argumentDefinition[0]->shorthelp = "The artist to search for";
+
+$output = new ezcConsoleOutput();
+$output->formats->info->color = 'blue';
+
+$output->formats->error->color = 'red';
+$output->formats->error->style = array( 'bold' );
+
+$output->formats->fatal->color = 'red';
+$output->formats->fatal->style = array( 'bold', 'underlined' );
+$output->formats->fatal->bgcolor = 'black';
 
 try
 {
 	$input->process();
+}
+catch ( ezcConsoleArgumentMandatoryViolationException $e )
+{
+	if ( $helpOption->value === true )
+	{
+		$output->outputText( $input->getHelpText( "Auto-link by artist name" ) );
+		exit;
+	}
+	else
+	{
+		$output->outputLine( "No arguments given", "fatal" );
+		$output->outputText( $input->getHelpText( "Auto-link by artist name" ) );
+		exit( 1 );
+	}
 }
 catch ( ezcConsoleException $e )
 {
 	die( $e->getMessage() );
 }
 
-if ( $helpOption->value === true )
+if ( $input->argumentDefinition["artist"]->value === null)
 {
-	echo $input->getHelpText( "Auto-link by artist name" );
+	$output->outputLine( "The <artist> argument is mandatory", 'fatal' );
+	$output->outputText( $input->getHelpText( "Auto-link by artist name" ), 'fatal' );
 	exit;
 }
 
-if ( $input->argumentDefinition["artist"]->value === null)
-{
-	echo "The <artist> argument is mandatory\n";
-	echo $input->getHelpText( "Auto-link by artist name" );
-	exit;
-}
+var_dump( $verboseOption->value );
 
 $it = new FOFSongIterator( '/home/xbmc/fof/songs/Packs' );
 $it->addFilter( 'artist', $input->argumentDefinition["artist"]->value );
 foreach ( $it as $song )
 {
 	$linkName = $song->name;
-	if ( !file_exists( $linkName ) )
+	if ( file_exists( $linkName ) and ( $verboseOption->value === true ) )
+	{
+		$output->outputLine( "$linkName already exists", "info" );
+	}
+	else
 	{
 		$path = $song->directory->getPathname();
 		if ( $quietOption !== false)
-			echo $path . " => " . $linkName. "\n";
+			$output->outputLine( "$linkName => $path" );
 		if ( $dryRunOption->value !== true )
 			symlink( $path, $linkName );
 	}
